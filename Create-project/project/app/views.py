c803706460c128715d0af4376c1a74f303a858ac
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-04-01 22:40:55 +0530

    new code

--- Create-project/project/app/views.py
+++ Create-project/project/app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     x="<h1 style='color:red'>Hello...</h1>"
-    return HttpResponse(x)
+    y="<div style='border:2px'>Hello sir..</div>"
+    return HttpResponse(y)
