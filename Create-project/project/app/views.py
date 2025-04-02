37c5beb6d3f92d44792c1ba19847460238df26d3
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-04-02 23:48:19 +0530

    new code

--- Create-project/project/app/views.py
+++ Create-project/project/app/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
 def home(request):
     x="<h1 style='color:red'>Hello...</h1>"
     y="<div style='border:2px'>Hello sir..</div>"
     return HttpResponse(y)
+
+def about(request):
+    data={'name':True,'age':False,'qual':None}
+    return JsonResponse(data)
+
+def index(request):
+    return render(request,'index.html')
+
+def new(request):
+    return redirect('https://www.google.com')
+
