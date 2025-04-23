7f416d2c8abe3042deaf00b48cab5e9ccf15d23b
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-04-23 19:25:48 +0530

    new code

---
+++ models abstract/project/app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+# Create your models here.
+
+class BaseInfo(models.Model):
+    name=models.CharField(max_length=30)
+    email=models.EmailField(max_length=255)
+    contact=models.IntegerField()
+    class Meta:
+        abstract=True
+    
+class Student(BaseInfo):
+    branch=models.CharField(max_length=15)
+    
+class Employee(BaseInfo):
+    department=models.CharField(max_length=15)
+    salary=models.IntegerField()
+    
+class Client(BaseInfo):
+    project=models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
--- models abstract/project/project/settings.py
+++ models abstract/project/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gj2ty#tl@#yi0$4_q0)h8wa26n!yzvhr+!fk-&o887s)e(lp%i'
+SECRET_KEY = 'django-insecure-%m0fhwu-q6p@jf33l0fyfbm5phb!j8ugrc96q6_fo(5ev_fjz%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
