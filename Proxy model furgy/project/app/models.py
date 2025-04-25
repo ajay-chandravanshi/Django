7fc596d54802a170609a9ef58c4ea37ddb442df9
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-04-25 23:55:20 +0530

    new code

--- Proxy model furgy/project/app/models.py
+++ Proxy model furgy/project/app/models.py
@@ -6,8 +6,6 @@ class MainModel(models.Model):
     name=models.CharField(max_length=50)
     email=models.EmailField(unique=True)
     contact=models.IntegerField()
-    department=models.CharField(max_length=50)
-    employeeid=models.IntegerField()
     address=models.CharField(max_length=50)
 
 class ProxyModel(MainModel):
