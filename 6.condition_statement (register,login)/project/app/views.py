baba0be3fc8503d7a42a1eeb080df968a98e1b06
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-04-26 16:08:54 +0530

    new code

---
+++ 6.condition_statement (register,login)/project/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+def landing(request):
+    data={'name':'Ajay','age':23,'quali':'btech'}
+    return render(request,'home.html',{'data':data})
+def home(request):
+    return render(request,'home.html')
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return render(request,'contact.html')
+def login(request):
+    return render(request,'login.html')
+def register(request):
+    return render(request,'register.html')
