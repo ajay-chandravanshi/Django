dbf5a7e5eb9b6c1ad366e25b3c3ca4aef053cc28
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-05-01 19:26:58 +0530

    new code

---
+++ 12.New folder/project/app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Aadhar(models.Model):
+    adharno=models.IntegerField(unique=True)
+    crteatedby=models.CharField(max_length=40)
+
+    def __str__(self):
+        return str(self.adharno)
+
+class Student(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(unique=True)
+    contact=models.IntegerField()
+    roll=models.IntegerField()
+    aadhar=models.OneToOneField(Aadhar,on_delete=models.PROTECT,to_field="adharno")
