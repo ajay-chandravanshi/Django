a1d8f0d422b97387b01697888971d6b55d120525
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-05-06 19:35:06 +0530

    new code

---
+++ 14.models relation many to many/project/app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+# Create your models here.
+
+class Fuel(models.Model):
+    Fuel_name=models.CharField(unique=True,max_length=50)
+    def __str__(self):
+        return self.Fuel_name
+
+class Vehicles(models.Model):
+    Vehicle_name=models.CharField(max_length=50)
+    Fuel_name = models.ManyToManyField(Fuel,related_name='vehical')
+    def __str__(self):
+        return self.Vehicle_name
+   
