036600cbcd73502a4e4404d008912303ff7cbef6
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-05-10 21:53:39 +0530

    new code

--- 16.query/project/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Student(models.Model):
-    name=models.CharField(max_length=50)
-    email=models.EmailField()
-    contact=models.IntegerField()
-    
