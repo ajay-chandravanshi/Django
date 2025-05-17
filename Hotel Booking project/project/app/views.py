f0099f90e91a2c0eb5f673bcc25777160ddf6b23
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-05-17 23:34:22 +0530

    new code

--- Hotel Booking project/project/app/views.py
+++ Hotel Booking project/project/app/views.py
@@ -145,4 +145,25 @@ def allquery(request,pk):
     x=userdata.clt_email
     querydetail=Query.objects.filter(stu_email=x)
     return render(request,'dashboard.html',{'userdata':userdata,'querydetail':querydetail})
+
+def edit(request,pk):
+    editdata=Query.objects.get(id=pk)
+    email=editdata.stu_email
+    userdata=Client.objects.get(clt_email=email)
+    return render(request,'dashboard.html',{'userdata':userdata,'editdata':editdata})
+
+def queryupdate(request,pk):
+    if request.method=='POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        query=request.POST.get('query')
+        old_query=Query.objects.get(id=pk)
+        old_query.stu_name=name
+        old_query.stu_email=email
+        old_query.stu_query=query
+        old_query.save()
+        querydetail=Query.objects.filter(stu_email=email)
+        userdata=Client.objects.get(clt_email=email)
+'querydetail':querydetail,'msg': msg, 'msg_type': 'success'})
+
 # for query code End
