f070c94d7f19c98aa56d1620bcae497f87134bcb
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-05-21 23:44:03 +0530

    new code

--- 18.Session and cookies/project/app/views.py
+++ 18.Session and cookies/project/app/views.py
@@ -19,13 +19,38 @@ def register(req):
         x.set_cookie("name",uname)
         x.set_cookie('passw',passw)
         x.set_cookie('dob',dob)
-        x.set_cookie('dob',email)
-        x.set_cookie('dob',phone)
+        x.set_cookie('email',email)
+        x.set_cookie('phone',phone)
         return x
 
+def login(req):
+    if req.method=='POST':
+        x = req.POST.get('email')
+        p=req.POST.get('password')
+        n=req.COOKIES['name']
+        pa=req.COOKIES['passw']
+        do=req.COOKIES['dob']
+        e=req.COOKIES['email']
+        ph=req.COOKIES['phone']
+        userdata={'name':n,'dob':do,'email':e,'phone':ph}
+        print(ph,n,do,e,pa)
+        print(x,p)
+        if (x==e and p==pa):
+            return render(req,'app/dashboard.html',{'userdata':userdata})
+        else:
+            return render(req,'app/login.html')
 
+    else:
+        return render(req,'app/login.html')
 
-
+def logout(request):
+    resp = render(request,'app/register.html')
+    resp.delete_cookie('name')
+    resp.delete_cookie('passw')
+    resp.delete_cookie('dob')
+    resp.delete_cookie('email')
+    resp.delete_cookie('phone')
+    return resp
 
 
 
