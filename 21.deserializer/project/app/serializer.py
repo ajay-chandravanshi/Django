ecdc4dcd73b78f536a947f264a9a14255fa172ff
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-06-03 23:40:13 +0530

    new code

--- 21.deserializer/project/app/serializer.py
+++ 21.deserializer/project/app/serializer.py
@@ -18,3 +18,14 @@ class StudentSerializer(serializers.Serializer):
     #     instance.save()
     #     return instance
 
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
+
+
+    def update(self, instance, validated_data):
+        # instance.id = validated_data.get('id', instance.id)
+        instance.name = validated_data.get('name', instance.name)
+        instance.city = validated_data.get('city', instance.city)
+        instance.roll = validated_data.get('roll', instance.roll)
+        instance.save()
+        return instance
