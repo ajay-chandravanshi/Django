6495f4f9d4b3dcded20fddc4569e5877df4aa6d7
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-06-06 00:03:11 +0530

    new code

--- 22.API using DRF/project/app/views.py
+++ 22.API using DRF/project/app/views.py
@@ -1,3 +1,39 @@
+from .models import Student
+from .serializer import StudentSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+# Create your views here.
+
+@api_view(['GET','POST'])
+def student_list(request):
+    if request.method=='GET':
+        student = Student.objects.all()
+        serializer = StudentSerializer(student, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+@api_view(['GET','PUT','DELETE','PATCH'])
+
+def student_detail(request,pk):
+    id=Student.objects.filter(id=pk)
+    if id:
+        if request.method=='GET':
+            Student = Student.objects.get(id=pk)
+            serializer = student_detail(Student)
+            return Response(serializer.data)
+        
+        elif request.method=='PUT':
+            Student = Student.objects.get(id=pk)
+            serializer = StudentSerializer(Student, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors)
         
 
