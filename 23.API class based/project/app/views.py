dda83a0b77a0aee75e026df374cb4810b6d40143
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-06-06 19:27:30 +0530

    new code

--- 23.API class based/project/app/views.py
+++ 23.API class based/project/app/views.py
@@ -6,7 +6,38 @@ from rest_framework.response import Response
 from rest_framework import status
 
 class student_detail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            raise Http404
 
+    def get(self, request, pk, format=None):
+        student = self.get_object(pk)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        student = self.get_object(pk)
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def patch(self, request, pk, format=None):
+        student = self.get_object(pk)
+        serializer = StudentSerializer(student, data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
 
