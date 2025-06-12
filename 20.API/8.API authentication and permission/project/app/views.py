83097e3e238bb524dd6c1545fc40ab01b20224ee
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-06-12 23:47:01 +0530

    new code

--- 20.API/8.API authentication and permission/project/app/views.py
+++ 20.API/8.API authentication and permission/project/app/views.py
@@ -11,5 +11,7 @@ class StudentViewSets(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated] 
     # permission_classes = [AllowAny] 
     # permission_classes = [IsAdminUser] 
-   
+    permission_classes = [IsAuthenticatedOrReadOnly] 
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer  
         
