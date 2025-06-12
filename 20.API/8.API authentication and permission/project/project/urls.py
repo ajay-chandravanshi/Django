d25659f28c0fb35d33720ca6b0787590be174cb0
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-06-12 23:47:18 +0530

    new code

--- 20.API/8.API authentication and permission/project/project/urls.py
+++ 20.API/8.API authentication and permission/project/project/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)), 
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    
+    path('api-token-auth/', views.obtain_auth_token)
     
 ]
