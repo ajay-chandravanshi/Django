fde1a01082628e1409706b6b494b05d462e25e21
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-06-13 23:43:48 +0530

    new code

--- 20.API/8.API authentication and permission/project/project/urls.py
+++ 20.API/8.API authentication and permission/project/project/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api-token-auth/', views.obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
