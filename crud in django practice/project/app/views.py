d6c106fd520d8d6fa8314d59b1a30dd73a0f635f
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-06-20 22:49:48 +0530

    new code

--- crud in django practice/project/app/views.py
+++ crud in django practice/project/app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Student
 # Create your views here.
+
+def register(request):
+    if request.method=='post':
+        uname=request.POST.get('name')
+        uemail=request.POST.get('email')
+        ucity=request.POST.get('city')
+        Student.objects.create(name=uname,email=uemail,city=ucity)
+        return redirect('dashboard')
+    return render(request,'register.html')
