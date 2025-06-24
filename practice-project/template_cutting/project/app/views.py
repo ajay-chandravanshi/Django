b5b6dde15012286a1676f68169d6d6b53ce32fc0
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-06-24 23:01:50 +0530

    new code

--- practice-project/template_cutting/project/app/views.py
+++ practice-project/template_cutting/project/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+def login(request):
+    return render(request,'login.html')
+def register(request):
+    return render(request,'register.html')
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return render(request,'contact.html')
