8a817c139bfdb50be4595719721829e531c0a3dd
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-07-02 22:58:30 +0530

    new code

--- practice-project/crud/project/app/views.py
+++ practice-project/crud/project/app/views.py
@@ -14,6 +14,3 @@ def register(request):
 def dashboard(request):
     data=Student.objects.all()
     return render(request,'dashboard.html',{'data':data})
-def dashboard(request):
-    data=Student.objects.all()
-    return render(request,'dashboard.html',{'data':data})
